e33bfa5e91fc1905cad0b8217bbc6abec1441584
Krzysztof Tybinka <ktybinka@interia.pl>
2023-11-22 23:22:48 +0100

    weather request

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
+const key = 'b3ae74932b305e9002676ce8ef72bfbb';
+let todayInfo = {}
 
-async function searchCity(input) {
+async function onCitySearch(input) {
 
     if (input.value.length >= 1) {
         const cities = await matchCities(input.value)
         $('#search-box-list').empty();
 
         for (city of cities) {
-            $('#search-box-list').append(`<li onclick="chooseCity(this)" lat="${city.lat}" lng="${city.lon}">${city.name} ${city.country_code} ${city.timezone}</li>`);
+            $('#search-box-list').append(`<li onclick="onCityChosen(this)" lat="${city.lat}" lon="${city.lon}">${city.name} ${city.country_code} ${city.timezone}</li>`);
         }
     }
     else {
@@ -21,16 +23,27 @@ async function matchCities(input) {
 }
 
 
-function chooseCity(city) {
+function onCityChosen(city) {
     $('#search-box-list').empty();
     $('#search-box-input').val(city.innerHTML)
+
+    let lat = $(city).attr('lat')
+    let lon = $(city).attr('lon')
+    getWeather(lat, lon)
 }
 
-
-async function getCitiesJson(cityName) {
-    const response = await fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=5&appid=b3ae74932b305e9002676ce8ef72bfbb");
-    const cityData = await response.json();
-    return cityData;
+async function getWeather(lat, lon) {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&units=metric`);
+    const weather = await response.json();
+    debugger;
+    todayInfo = {
+        image: "",
+        currentInfo: {
+            cityName: weather.name,
+            description: weather[0].description,
+            degrees: weather.main.temp
+        },
+    }
 }
 
 
