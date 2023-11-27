4c626ea58067eb29fc5a1e7f726a3fb9e95261d8
Krzysztof Tybinka <ktybinka@interia.pl>
2023-11-27 18:55:15 +0100

    5day weather

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const key = 'b3ae74932b305e9002676ce8ef72bfbb';
-let todayInfo = {}
+
+let weatherInfo = {
+    cityName: null,
+    info: []
+};
 
 async function onCitySearch(input) {
 
@@ -23,27 +27,110 @@ async function matchCities(input) {
 }
 
 
-function onCityChosen(city) {
-    $('#search-box-list').empty();
-    $('#search-box-input').val(city.innerHTML)
+async function onCityChosen(city) {
+    clearAll();
 
-    let lat = $(city).attr('lat')
-    let lon = $(city).attr('lon')
-    getWeather(lat, lon)
+    $('#search-box-list').empty();
+    $('#search-box-input').val(city.innerHTML);
+
+    let lat = $(city).attr('lat');
+    let lon = $(city).attr('lon');
+    await getWeather(lat, lon);
+    fillWeekInfo();
 }
 
+function fillWeekInfo() {
+    weatherInfo.info.forEach(element => {
+        $('#week-list').append(`<li>${element.date} ${element.average} ${element.maxTemp}/${element.minTemp}</li>`);
+    });
+}
+
+function clearAll() {
+    $('#week-list').empty();
+    weatherInfo = {
+        cityName: null,
+        info: []
+    };
+}
+
+
 async function getWeather(lat, lon) {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&units=metric`);
-    const weather = await response.json();
-    debugger;
-    todayInfo = {
-        image: "",
-        currentInfo: {
-            cityName: weather.name,
-            description: weather[0].description,
-            degrees: weather.main.temp
-        },
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}&units=metric`);
+    weather = await response.json();
+
+    weatherInfo.cityName = weather.city.name;
+    let degreeArr = [];
+    let oneDay = [];
+    let descArr = [];
+    let counter = 0;
+
+    weather.list.forEach(element => {
+
+        let info = {
+            hour: new Date(element.dt_txt).getHours() + ":00",
+            temp: Math.round(element.main.temp),
+            description: element.weather[0].description,
+            details: {
+                feelsLike: Math.round(element.main.feels_like),
+                humidity: element.main.humidity,
+                pressure: element.main.pressure,
+                windSpeed: element.wind.speed
+            }
+        };
+        degreeArr.push(info.temp)
+        descArr.push(info.description)
+        oneDay.push(info);
+
+        if ((counter + 1) % 8 === 0) {
+            degreeArr.sort()
+
+            weatherInfo.info.push({
+                date: day(element.dt_txt),
+                average: mostFrequent(descArr),
+                maxTemp: Math.round(degreeArr.lastIndexOf()),
+                minTemp: Math.round(degreeArr[0]),
+                dayInfo: oneDay
+            });
+
+            degreeArr = [];
+            oneDay = [];
+            descArr = [];
+        }
+        counter++;
+    });
+}
+
+function mostFrequent(arr) {
+    var mf = 1;
+    var m = 0;
+    var item;
+    for (var i = 0; i < arr.length; i++) {
+        for (var j = i; j < arr.length; j++) {
+            if (arr[i] == arr[j])
+                m++;
+            if (mf < m) {
+                mf = m;
+                item = arr[i];
+            }
+        }
+        m = 0;
     }
+    return item;
+}
+
+function day(date) {
+    let inputDate = new Date(date);
+    let todaysDate = new Date();
+    if (inputDate.getDay() == todaysDate.getDay()) {
+        return "Today";
+    }
+
+    const intDay = {
+        1: "Mon", 2: "Tue", 3: "Wed",
+        4: "Thu", 5: "Fri", 6: "Sat", 7: "Sun"
+    };
+    let ix = inputDate.getDay();
+    return intDay[ix];
 }
 
 
